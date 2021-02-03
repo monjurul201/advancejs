4f7b09361fd44f560085f95301065282b1429333
unknown <mojnushah2018@gmai.com>
2021-02-03 13:39:14 +0600

    true false

---
+++ trueFalse.js
@@ -0,0 +1,17 @@
+//false
+
+//""
+//0
+//null
+//NaN
+//undifined  
+
+//truth
+//" " ,"0" ,
+let age;
+if(age){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
