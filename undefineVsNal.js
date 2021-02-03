4b09dc773754759bc3c58f5e4f62325681f7324b
unknown <mojnushah2018@gmai.com>
2021-02-03 14:00:46 +0600

    undefinder vs null

---
+++ undefineVsNal.js
@@ -0,0 +1,16 @@
+
+
+// let phaki;
+// console.log(phaki);
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     // return 
+// }
+// const result =add(2,3);
+// console.log(result);
+
+// const phakhi={name:'phaki',age:15};
+// console.log(phakhi.gf);
+
+let fun =undefined;
+console.log(fun);
