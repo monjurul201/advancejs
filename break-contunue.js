d050fe4d1daa58ca1c1b2296c50649d469805e5f
unknown <mojnushah2018@gmai.com>
2021-02-04 20:16:34 +0600

    hello

---
+++ break-contunue.js
@@ -0,0 +1,7 @@
+// const num=[1,2,3,4,5,6,7,8,9];
+// for(let i=0;i<num.length;i++){
+//     console.log(num[i]);
+//     if(num[i]>3){
+//         break;
+//     }
+// }
---
+++ slice.js
@@ -0,0 +1,6 @@
+const number=[1,2,8,5,6,9,1,10,22];
+// const removeMiddle=number.slice(2,5);
+// console.log(removeMiddle);
+
+const remove=number.splice(3,5);
+console.log(remove);
