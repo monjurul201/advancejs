4553a6314532c5edc6b4101847def40387300307
unknown <mojnushah2018@gmai.com>
2021-02-04 17:29:17 +0600

    onek kicu shiklam

---
+++ object-property.js
@@ -0,0 +1,16 @@
+const student=[
+    {id:17,name:'omor sanny'},
+    {id:18,name:'amina'},
+    {id:19,name:'jamina'},
+    {id:77,name:'kamina'},
+    {id:88,name:'paibna'},
+
+];
+const names=student.map(s=>s.name);
+console.log(names);
+const bigger=student.find(s=>s.id>40);
+console.log(bigger);
+
+for(let i=0;i<student.length;i++){
+     
+}
